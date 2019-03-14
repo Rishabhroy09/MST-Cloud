13ad5878a393b0986b66845c2581a972c6a4fd0b
Rishabhroy09 <47408837+Rishabhroy09@users.noreply.github.com>
2019-03-14 09:56:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>FORM</title>
+</head>
+
+<body>
+<form >
+
+<p> UID <input type= "text" name="fname" placeholder="First Name ">
+<p> NAME <input type="text" name="lname" placeholder="Last Name ">
+<p> CLASS <input type="email"  name="email" placeholder="Enter Email">
+<p> UNIVERSITY <input type="password" name="pass" placeholder="Enter Password">
+ 
+ 
+ <p> HOBBIES <textarea placeholder="Enter Hobbies" name="address" rows="5" cols="20"></textarea>
+ 
+ <P> <input type="submit">
+ </form>
+
+</body>
+</html>
